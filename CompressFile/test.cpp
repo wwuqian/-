33bac7b8a9f1e01b73dc5d3f9b20a886e6c93516
wwuqian <33458946+wwuqian@users.noreply.github.com>
2018-08-10 17:11:19 +0800

    主函数

---
+++ CompressFile/test.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<assert.h>
+using namespace std;
+
+//#include"Heap.h"
+//#include "HuffmanTree.h"
+#include"FileCompress.hpp"
+
+
+int main()
+{
+	//TestHeap();
+	//TestHuffmanTree();
+	//TestreadLine();
+	//TestCompress();
+	TestUnCompress();
+	return 0;
+}
